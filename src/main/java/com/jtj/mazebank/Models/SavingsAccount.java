58c1f538f397ebdbee85d595f1218fb46124a77a
jjayoma2017 <jjayoma2017@gmail.com>
2024-02-08 23:40:32 +0800

    Created 3 new models

---
+++ src/main/java/com/jtj/mazebank/Models/SavingsAccount.java
@@ -0,0 +1,18 @@
+package com.jtj.mazebank.Models;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class SavingsAccount extends Account{
+    // The withdrawal limit from the savings
+    private final DoubleProperty withdrawalLimit;
+    public SavingsAccount(String owner, String accountNumber, double balance, double withdrawalLimit) {
+        super(owner, accountNumber, balance);
+        this.withdrawalLimit = new SimpleDoubleProperty(this,"Withdrawal Limit", withdrawalLimit);
+    }
+
+    public DoubleProperty withdrawalLimitProperty() {
+        return withdrawalLimit;
+    }
+
+}

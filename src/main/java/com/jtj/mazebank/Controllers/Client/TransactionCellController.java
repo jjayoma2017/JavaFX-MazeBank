d21fcda3e49d1ca583b994bd1ffd1b41b7373247
jjayoma2017 <jjayoma2017@gmail.com>
2024-02-09 16:40:44 +0800

    added cell icon

--- src/main/java/com/jtj/mazebank/Controllers/Client/TransactionCellController.java
+++ src/main/java/com/jtj/mazebank/Controllers/Client/TransactionCellController.java
@@ -1,9 +1,11 @@
 package com.jtj.mazebank.Controllers.Client;
 
+import com.jtj.mazebank.Models.Model;
 import com.jtj.mazebank.Models.Transaction;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -28,7 +30,17 @@ public class TransactionCellController implements Initializable {
         receiver_lbl.textProperty().bind(transaction.receiverProperty());
         amount_lbl.textProperty().bind(transaction.amountProperty().asString());
         trans_date_lbl.textProperty().bind(transaction.dateProperty().asString());
+        transactionIcons();
     }
 
-
+    private void transactionIcons(){
+        if(transaction.senderProperty().get().equals(Model.getInstance().getClient().payeeAddressProperty().get())){
+            in_icon.setFill(Color.rgb(240,240,240));
+            out_icon.setFill(Color.RED);
+        }
+        else {
+            in_icon.setFill(Color.GREEN);
+            out_icon.setFill(Color.rgb(240,240,240));
+        }
+    }
 }

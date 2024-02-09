1b3b65907316b8b35d5fd3bd08ee2da18fdffdfb
jjayoma2017 <jjayoma2017@gmail.com>
2024-02-09 16:31:28 +0800

    added account summary

--- src/main/java/com/jtj/mazebank/Controllers/Client/DashboardController.java
+++ src/main/java/com/jtj/mazebank/Controllers/Client/DashboardController.java
@@ -1,6 +1,7 @@
 package com.jtj.mazebank.Controllers.Client;
 
 import com.jtj.mazebank.Models.Model;
+import com.jtj.mazebank.Models.Transaction;
 import com.jtj.mazebank.Views.TransactionCellFactory;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.Initializable;
@@ -42,6 +43,7 @@ public class DashboardController implements Initializable {
                 throw new RuntimeException(ex);
             }
         });
+        accountSummary();
     }
 
     private void  bindData(){
@@ -84,5 +86,24 @@ public class DashboardController implements Initializable {
         payee_fld.setText("");
         amount_fld.setText("");
         message_fld.setText("");
+    }
+
+    // Method calculates all expenses and income
+    private void accountSummary(){
+        double income = 0;
+        double expenses = 0;
+        if(Model.getInstance().getAllTransactions().isEmpty()){
+            Model.getInstance().setAllTransactions();
+        }
+        for(Transaction transaction: Model.getInstance().getAllTransactions()){
+            if(transaction.senderProperty().get().equals(Model.getInstance().getClient().payeeAddressProperty().get())){
+                expenses = expenses + transaction.amountProperty().get();
+            }
+            else {
+                income = income + transaction.amountProperty().get();
+            }
+        }
+        income_lbl.setText("+ $"+income);
+        expense_lbl.setText("- $"+expenses);
     }
 }

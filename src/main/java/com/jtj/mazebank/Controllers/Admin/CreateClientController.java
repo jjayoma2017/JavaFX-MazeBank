bfca5e8498575eff5e7919dabfbe9a2d8c042ff4
jjayoma2017 <jjayoma2017@gmail.com>
2024-02-09 08:54:31 +0800

    added create DB methods

--- src/main/java/com/jtj/mazebank/Controllers/Admin/CreateClientController.java
+++ src/main/java/com/jtj/mazebank/Controllers/Admin/CreateClientController.java
@@ -1,5 +1,6 @@
 package com.jtj.mazebank.Controllers.Admin;
 
+import com.jtj.mazebank.Models.Model;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -7,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class CreateClientController implements Initializable {
@@ -22,8 +24,20 @@ public class CreateClientController implements Initializable {
     public Button create_client_btn;
     public Label error_lbl;
 
+    private String payeeAddress;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    private void createCheckingAccount(){
+        double balance = Double.parseDouble(ch_amount_fld.getText());
+        // Generate Account Number
+        String firstSection = "3201";
+        String lastSection = Integer.toString(new Random().nextInt(9999) + 1000);
+        String accountNumber = firstSection + " " +  lastSection;
+        // Create the checking Account
+        Model.getInstance().getDatabaseDriver().createCheckingAccount(payeeAddress,accountNumber,10,balance);
     }
 }
--- src/main/java/com/jtj/mazebank/Models/DatabaseDriver.java
+++ src/main/java/com/jtj/mazebank/Models/DatabaseDriver.java
@@ -1,6 +1,7 @@
 package com.jtj.mazebank.Models;
 
 import java.sql.*;
+import java.time.LocalDate;
 
 public class DatabaseDriver {
     private Connection conn;
@@ -45,6 +46,46 @@ public class DatabaseDriver {
         }
         return  resultSet;
     }
+
+    public void createClient(String fName, String lName, String pAddress, String password, LocalDate date){
+        Statement statement;
+        try{
+            statement = this.conn.createStatement();
+            statement.executeUpdate("INSERT INTO " +
+                    "Clients (FirstName, LastName, PayeeAddress, Password, Date) " +
+                    "VALUES ('" + fName + "' , '" + lName + "', '" + pAddress + "','" + password + "','" + date.toString() + "')");
+        }
+        catch (SQLException e){
+            throw  new RuntimeException(e);
+        }
+    }
+
+    public void createCheckingAccount(String owner, String number, double tLimit, double balance){
+        Statement statement;
+        try{
+            statement = this.conn.createStatement();
+            statement.executeUpdate("INSERT INTO " +
+                    "CheckingAccounts (Owner, AccountNumber, TransactionLimit, Balance) " +
+                    "VALUES ('" + owner + "' , '" + number + "', '" + tLimit + "','" + balance + "')");
+        }
+        catch (SQLException e){
+            throw  new RuntimeException(e);
+        }
+    }
+
+    public void createSavingsAccount(String owner, String number, double wLimit, double balance){
+        Statement statement;
+        try{
+            statement = this.conn.createStatement();
+            statement.executeUpdate("INSERT INTO " +
+                    "SavingsAccounts (Owner, AccountNumber, WithdrwalLimit, Balance) " +
+                    "VALUES ('" + owner + "' , '" + number + "', '" + wLimit + "','" + balance + "')");
+        }
+        catch (SQLException e){
+            throw  new RuntimeException(e);
+        }
+    }
+
     /*
     * Utility Methods
     * */
